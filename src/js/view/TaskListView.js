7bb11d2c6d88c9ef4039bd07f66b5e840a0ab91c
My420 <my420V@gmail.com>
2019-10-07 23:44:59 +0300

    add mouse dnd prototype

--- src/js/view/TaskListView.js
+++ src/js/view/TaskListView.js
@@ -12,6 +12,7 @@ class TaskListView extends AbstractView {
     this._emptyMessageTemplate = '<li class="taskList__message">задач нет</li>';
     this._onUserClick = this._onUserClick.bind(this);
     this._onKeyUp = this._onKeyUp.bind(this);
+    this._onMouseDown = this._onMouseDown.bind(this);
   }
 
   _findTask(id) {
@@ -21,8 +22,8 @@ class TaskListView extends AbstractView {
   _getListTemplate() {
     if (this._tasks.length > 0) {
       const template = this._tasks
-        .map(elem => {
-          return taskItemTemplate(elem);
+        .map((elem, i) => {
+          return taskItemTemplate(elem, i);
         })
         .join('');
 
@@ -96,15 +97,105 @@ class TaskListView extends AbstractView {
     oldTaskElement.replaceWith(newTaskElement);
   }
 
+  _onMouseDown(evt) {
+    if (evt.target.className === 'taskList__screen') {
+      const taskIndex = evt.target.dataset.index;
+      let isMouseMove = false;
+      const shiftX = evt.clientX - evt.target.getBoundingClientRect().left;
+      const shiftY = evt.clientY - evt.target.getBoundingClientRect().top;
+
+      const startPositionX = evt.clientX;
+      const startPositionY = evt.clientY;
+
+      const { offsetWidth: width, offsetHeight: height } = evt.target;
+      let currentDroppable = null;
+
+      const clone = evt.target.parentNode.cloneNode(true);
+      clone.style.width = `${width}px`;
+      clone.style.height = `${height}px`;
+      clone.style.zIndex = 1000;
+      clone.style.position = 'absolute';
+
+      const moveAt = (pageX, pageY) => {
+        clone.style.left = `${pageX - shiftX}px`;
+        clone.style.top = `${pageY - shiftY}px`;
+      };
+
+      const onMouseMove = event => {
+        const posShiftX = startPositionX - event.pageX;
+        const posShiftY = startPositionY - event.pageY;
+
+        if (Math.abs(posShiftX) > 3 || Math.abs(posShiftY) > 3) {
+          isMouseMove = true;
+          document.body.append(clone);
+          moveAt(event.pageX, event.pageY);
+
+          clone.hidden = true;
+          const elemBelow = document.elementFromPoint(
+            event.clientX,
+            event.clientY
+          );
+          clone.hidden = false;
+          moveAt(event.pageX, event.pageY);
+
+          if (!elemBelow) return;
+          const droppableBelow =
+            elemBelow.closest('.calendar__screen') ||
+            elemBelow.closest('.taskList__screen');
+
+          if (currentDroppable !== droppableBelow) {
+            if (currentDroppable) {
+              console.log(`улетели с `, currentDroppable);
+            }
+
+            currentDroppable = droppableBelow;
+
+            if (droppableBelow) {
+              console.log(`прилетели на `, droppableBelow.dataset.date);
+            }
+          }
+        }
+      };
+
+      const onMouseUp = () => {
+        console.log(currentDroppable);
+
+        if (!currentDroppable) {
+          if (isMouseMove) {
+            clone.remove();
+          } else {
+            this._onUserClick(evt);
+          }
+        } else if (currentDroppable.classList.value === 'calendar__screen') {
+          console.log(this._tasks[taskIndex]);
+          console.log(`мигрирум на ${currentDroppable.dataset.date}`);
+          clone.remove();
+        } else {
+          if (evt.target.dataset.date === currentDroppable.dataset.date) {
+            this._onUserClick(evt);
+          }
+          clone.remove();
+        }
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+  }
+
   bind() {
     this._taskList = this.element.querySelector('.taskList__list');
-    this._taskList.addEventListener('click', this._onUserClick);
+    // this._taskList.addEventListener('click', this._onUserClick);
     this._taskList.addEventListener('keyup', this._onKeyUp);
+    this._taskList.addEventListener('mousedown', this._onMouseDown);
   }
 
   unbind() {
-    this._taskList.removeEventListener('click', this._onUserClick);
+    // this._taskList.removeEventListener('click', this._onUserClick);
     this._taskList.removeEventListener('keyup', this._onKeyUp);
+    this._taskList.removeEventListener('mousedown', this._onMouseDown);
   }
 
   onTaskClick() {}
--- src/js/view/taskItemTemplate.js
+++ src/js/view/taskItemTemplate.js
@@ -1,8 +1,8 @@
-const taskItemTemplate = task => {
+const taskItemTemplate = (task, i) => {
   const { taskColor, taskName, taskStatus, taskDate, taskId } = task;
 
   return `<li class="taskList__item" data-id=${taskId} data-date=${taskDate}>
-  <div class="taskList__screen" data-id=${taskId} data-date=${taskDate} tabindex='0'></div>
+  <div class="taskList__screen" data-id=${taskId} data-date=${taskDate} data-index=${i} tabindex='0'></div>
   <div class="taskList__wrapper">    
     <div class="taskList__color" data-color=${taskColor} data-status=${taskStatus}></div>
     <div class="taskList__inner">
