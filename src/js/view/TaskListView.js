1f38b732e38cbb35e7d46b830cf37545e1bbac37
My420 <my420V@gmail.com>
2019-10-10 11:46:24 +0300

    complete dnd for touchscreen

--- src/js/view/TaskListView.js
+++ src/js/view/TaskListView.js
@@ -13,6 +13,7 @@ class TaskListView extends AbstractView {
     this._onUserClick = this._onUserClick.bind(this);
     this._onKeyUp = this._onKeyUp.bind(this);
     this._onMouseDown = this._onMouseDown.bind(this);
+    this._onTouchStart = this._onTouchStart.bind(this);
   }
 
   _findTask(id) {
@@ -98,6 +99,7 @@ class TaskListView extends AbstractView {
   }
 
   _onMouseDown(evt) {
+    console.log(`mouseDown`);
     if (evt.target.className === 'taskList__screen' && evt.button < 2) {
       evt.preventDefault();
 
@@ -189,17 +191,112 @@ class TaskListView extends AbstractView {
     }
   }
 
+  _onTouchStart(evt) {
+    console.log(`touchstart`);
+    evt.preventDefault();
+    if (evt.target.className === 'taskList__screen') {
+      const touch = evt.changedTouches[0];
+
+      const screen = touch.target;
+      const task = screen.parentNode;
+
+      let isTouchMove = false;
+      const startPositionX = touch.clientX;
+      const startPositionY = touch.clientY;
+      const shiftX = startPositionX - screen.getBoundingClientRect().left;
+      const shiftY = startPositionY - screen.getBoundingClientRect().top;
+      const { offsetWidth: width, offsetHeight: height } = screen;
+      let currentDroppable = null;
+
+      const clone = task.cloneNode(true);
+      clone.style.width = `${width}px`;
+      clone.style.height = `${height}px`;
+
+      const moveAt = (pageX, pageY) => {
+        clone.style.left = `${pageX - shiftX}px`;
+        clone.style.top = `${pageY - shiftY}px`;
+      };
+
+      const onTouchMove = event => {
+        const newTouch = event.changedTouches[0];
+        const posShiftX = startPositionX - newTouch.pageX;
+        const posShiftY = startPositionY - newTouch.pageY;
+
+        if (Math.abs(posShiftX) > 3 || Math.abs(posShiftY) > 3) {
+          isTouchMove = true;
+          task.hidden = true;
+          document.body.append(clone);
+          moveAt(newTouch.pageX, newTouch.pageY);
+
+          clone.hidden = true;
+          const elemBelow = document.elementFromPoint(
+            newTouch.clientX,
+            newTouch.clientY
+          );
+          clone.hidden = false;
+          moveAt(newTouch.pageX, newTouch.pageY);
+
+          if (!elemBelow) return;
+          const droppableBelow = elemBelow.closest('.calendar__screen');
+
+          if (currentDroppable !== droppableBelow) {
+            if (currentDroppable) {
+              currentDroppable.dataset.droppable = false;
+            }
+
+            currentDroppable = droppableBelow;
+
+            if (droppableBelow) {
+              currentDroppable.dataset.droppable = true;
+            }
+          }
+        }
+      };
+
+      const onTouchEnd = () => {
+        if (!currentDroppable) {
+          if (isTouchMove) {
+            clone.remove();
+            task.hidden = false;
+          } else {
+            this._onUserClick(touch);
+          }
+        } else {
+          currentDroppable.dataset.droppable = false;
+          clone.remove();
+
+          const taskIndex = screen.dataset.index;
+          const oldTask = this._tasks[taskIndex];
+          const oldDate = oldTask.taskDate;
+          const newDate = currentDroppable.dataset.date;
+          if (newDate === oldDate) {
+            console.log('даты равны');
+            task.hidden = false;
+          } else {
+            const newTask = { ...oldTask, taskDate: newDate };
+            console.log(oldTask, newTask);
+          }
+        }
+        document.removeEventListener('touchmove', onTouchMove);
+        document.removeEventListener('touchend', onTouchEnd);
+      };
+
+      document.addEventListener('touchmove', onTouchMove);
+      document.addEventListener('touchend', onTouchEnd);
+    }
+  }
+
   bind() {
     this._taskList = this.element.querySelector('.taskList__list');
-    // this._taskList.addEventListener('click', this._onUserClick);
     this._taskList.addEventListener('keyup', this._onKeyUp);
     this._taskList.addEventListener('mousedown', this._onMouseDown);
+    this._taskList.addEventListener('touchstart', this._onTouchStart);
   }
 
   unbind() {
-    // this._taskList.removeEventListener('click', this._onUserClick);
     this._taskList.removeEventListener('keyup', this._onKeyUp);
     this._taskList.removeEventListener('mousedown', this._onMouseDown);
+    this._taskList.removeEventListener('touchstart', this._onTouchStart);
   }
 
   onTaskClick() {}
