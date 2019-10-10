f55548c2dc7223b555f7f3cd2a109db7a79cb36f
My420 <my420V@gmail.com>
2019-10-10 14:21:04 +0300

    refactoring dnd

--- src/js/view/TaskListView.js
+++ src/js/view/TaskListView.js
@@ -12,8 +12,8 @@ class TaskListView extends AbstractView {
     this._emptyMessageTemplate = '<li class="taskList__message">задач нет</li>';
     this._onUserClick = this._onUserClick.bind(this);
     this._onKeyUp = this._onKeyUp.bind(this);
-    this._onMouseDown = this._onMouseDown.bind(this);
-    this._onTouchStart = this._onTouchStart.bind(this);
+    this._onUserPress = this._onUserPress.bind(this);
+    this._onPress = this._onPress.bind(this);
   }
 
   _findTask(id) {
@@ -54,6 +54,117 @@ class TaskListView extends AbstractView {
       const { date } = evt.target.dataset;
       const { id } = evt.target.dataset;
       this.onTaskClick(date, id);
+    }
+  }
+
+  _onUserPress(evt, isMouse = true) {
+    evt.preventDefault();
+    const pressEvt = isMouse ? evt : evt.changedTouches[0];
+
+    const screen = pressEvt.target;
+    const task = screen.parentNode;
+
+    let isMove = false;
+    const startPositionX = pressEvt.clientX;
+    const startPositionY = pressEvt.clientY;
+    const shiftX = startPositionX - screen.getBoundingClientRect().left;
+    const shiftY = startPositionY - screen.getBoundingClientRect().top;
+    const { offsetWidth: width, offsetHeight: height } = screen;
+    let currentDroppable = null;
+
+    const clone = task.cloneNode(true);
+    clone.style.width = `${width}px`;
+    clone.style.height = `${height}px`;
+
+    const moveAt = (pageX, pageY) => {
+      clone.style.left = `${pageX - shiftX}px`;
+      clone.style.top = `${pageY - shiftY}px`;
+    };
+
+    const onMove = event => {
+      const moveEvt = isMouse ? event : event.changedTouches[0];
+      const posShiftX = startPositionX - moveEvt.pageX;
+      const posShiftY = startPositionY - moveEvt.pageY;
+
+      if (Math.abs(posShiftX) > 3 || Math.abs(posShiftY) > 3) {
+        isMove = true;
+        task.hidden = true;
+        document.body.append(clone);
+        moveAt(moveEvt.pageX, moveEvt.pageY);
+
+        clone.hidden = true;
+        const elemBelow = document.elementFromPoint(
+          moveEvt.clientX,
+          moveEvt.clientY
+        );
+        clone.hidden = false;
+        moveAt(moveEvt.pageX, moveEvt.pageY);
+
+        if (!elemBelow) return;
+        const droppableBelow = elemBelow.closest('.calendar__screen');
+
+        if (currentDroppable !== droppableBelow) {
+          if (currentDroppable) {
+            currentDroppable.dataset.droppable = false;
+          }
+
+          currentDroppable = droppableBelow;
+
+          if (droppableBelow) {
+            currentDroppable.dataset.droppable = true;
+          }
+        }
+      }
+    };
+
+    const onEnd = () => {
+      if (!currentDroppable) {
+        if (isMove) {
+          clone.remove();
+          task.hidden = false;
+        } else {
+          this._onUserClick(pressEvt);
+        }
+      } else {
+        currentDroppable.dataset.droppable = false;
+        clone.remove();
+
+        const taskIndex = screen.dataset.index;
+        const oldTask = this._tasks[taskIndex];
+        const oldDate = oldTask.taskDate;
+        const newDate = currentDroppable.dataset.date;
+        if (newDate === oldDate) {
+          task.hidden = false;
+        } else {
+          const newTask = { ...oldTask, taskDate: newDate };
+          console.log(oldTask, newTask);
+        }
+      }
+      if (isMouse) {
+        document.removeEventListener('mousemove', onMove);
+        document.removeEventListener('mouseup', onEnd);
+      } else {
+        document.removeEventListener('touchmove', onMove);
+        document.removeEventListener('touchend', onEnd);
+      }
+    };
+
+    if (isMouse) {
+      document.addEventListener('mousemove', onMove);
+      document.addEventListener('mouseup', onEnd);
+    } else {
+      document.addEventListener('touchmove', onMove);
+      document.addEventListener('touchend', onEnd);
+    }
+  }
+
+  _onPress(evt) {
+    if (evt.target.className === 'taskList__screen') {
+      if (evt.type === 'mousedown' && evt.button < 2) {
+        this._onUserPress(evt, true);
+      } else if (evt.type === 'touchstart') {
+        this._onUserPress(evt, false);
+      }
     }
   }
 
@@ -98,205 +209,17 @@ class TaskListView extends AbstractView {
     oldTaskElement.replaceWith(newTaskElement);
   }
 
-  _onMouseDown(evt) {
-    console.log(`mouseDown`);
-    if (evt.target.className === 'taskList__screen' && evt.button < 2) {
-      evt.preventDefault();
-
-      const screen = evt.target;
-      const task = screen.parentNode;
-
-      let isMouseMove = false;
-      const startPositionX = evt.clientX;
-      const startPositionY = evt.clientY;
-      const shiftX = startPositionX - screen.getBoundingClientRect().left;
-      const shiftY = startPositionY - screen.getBoundingClientRect().top;
-      const { offsetWidth: width, offsetHeight: height } = screen;
-      let currentDroppable = null;
-
-      const clone = task.cloneNode(true);
-      clone.style.width = `${width}px`;
-      clone.style.height = `${height}px`;
-
-      const moveAt = (pageX, pageY) => {
-        clone.style.left = `${pageX - shiftX}px`;
-        clone.style.top = `${pageY - shiftY}px`;
-      };
-
-      const onMouseMove = event => {
-        const posShiftX = startPositionX - event.pageX;
-        const posShiftY = startPositionY - event.pageY;
-
-        if (Math.abs(posShiftX) > 3 || Math.abs(posShiftY) > 3) {
-          isMouseMove = true;
-          task.hidden = true;
-          document.body.append(clone);
-          moveAt(event.pageX, event.pageY);
-
-          clone.hidden = true;
-          const elemBelow = document.elementFromPoint(
-            event.clientX,
-            event.clientY
-          );
-          clone.hidden = false;
-          moveAt(event.pageX, event.pageY);
-
-          if (!elemBelow) return;
-          const droppableBelow = elemBelow.closest('.calendar__screen');
-
-          if (currentDroppable !== droppableBelow) {
-            if (currentDroppable) {
-              currentDroppable.dataset.droppable = false;
-            }
-
-            currentDroppable = droppableBelow;
-
-            if (droppableBelow) {
-              currentDroppable.dataset.droppable = true;
-            }
-          }
-        }
-      };
-
-      const onMouseUp = () => {
-        if (!currentDroppable) {
-          if (isMouseMove) {
-            clone.remove();
-            task.hidden = false;
-          } else {
-            this._onUserClick(evt);
-          }
-        } else {
-          currentDroppable.dataset.droppable = false;
-          clone.remove();
-
-          const taskIndex = screen.dataset.index;
-          const oldTask = this._tasks[taskIndex];
-          const oldDate = oldTask.taskDate;
-          const newDate = currentDroppable.dataset.date;
-          if (newDate === oldDate) {
-            console.log('даты равны');
-            task.hidden = false;
-          } else {
-            const newTask = { ...oldTask, taskDate: newDate };
-            console.log(oldTask, newTask);
-          }
-        }
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      };
-
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-    }
-  }
-
-  _onTouchStart(evt) {
-    console.log(`touchstart`);
-    evt.preventDefault();
-    if (evt.target.className === 'taskList__screen') {
-      const touch = evt.changedTouches[0];
-
-      const screen = touch.target;
-      const task = screen.parentNode;
-
-      let isTouchMove = false;
-      const startPositionX = touch.clientX;
-      const startPositionY = touch.clientY;
-      const shiftX = startPositionX - screen.getBoundingClientRect().left;
-      const shiftY = startPositionY - screen.getBoundingClientRect().top;
-      const { offsetWidth: width, offsetHeight: height } = screen;
-      let currentDroppable = null;
-
-      const clone = task.cloneNode(true);
-      clone.style.width = `${width}px`;
-      clone.style.height = `${height}px`;
-
-      const moveAt = (pageX, pageY) => {
-        clone.style.left = `${pageX - shiftX}px`;
-        clone.style.top = `${pageY - shiftY}px`;
-      };
-
-      const onTouchMove = event => {
-        const newTouch = event.changedTouches[0];
-        const posShiftX = startPositionX - newTouch.pageX;
-        const posShiftY = startPositionY - newTouch.pageY;
-
-        if (Math.abs(posShiftX) > 3 || Math.abs(posShiftY) > 3) {
-          isTouchMove = true;
-          task.hidden = true;
-          document.body.append(clone);
-          moveAt(newTouch.pageX, newTouch.pageY);
-
-          clone.hidden = true;
-          const elemBelow = document.elementFromPoint(
-            newTouch.clientX,
-            newTouch.clientY
-          );
-          clone.hidden = false;
-          moveAt(newTouch.pageX, newTouch.pageY);
-
-          if (!elemBelow) return;
-          const droppableBelow = elemBelow.closest('.calendar__screen');
-
-          if (currentDroppable !== droppableBelow) {
-            if (currentDroppable) {
-              currentDroppable.dataset.droppable = false;
-            }
-
-            currentDroppable = droppableBelow;
-
-            if (droppableBelow) {
-              currentDroppable.dataset.droppable = true;
-            }
-          }
-        }
-      };
-
-      const onTouchEnd = () => {
-        if (!currentDroppable) {
-          if (isTouchMove) {
-            clone.remove();
-            task.hidden = false;
-          } else {
-            this._onUserClick(touch);
-          }
-        } else {
-          currentDroppable.dataset.droppable = false;
-          clone.remove();
-
-          const taskIndex = screen.dataset.index;
-          const oldTask = this._tasks[taskIndex];
-          const oldDate = oldTask.taskDate;
-          const newDate = currentDroppable.dataset.date;
-          if (newDate === oldDate) {
-            console.log('даты равны');
-            task.hidden = false;
-          } else {
-            const newTask = { ...oldTask, taskDate: newDate };
-            console.log(oldTask, newTask);
-          }
-        }
-        document.removeEventListener('touchmove', onTouchMove);
-        document.removeEventListener('touchend', onTouchEnd);
-      };
-
-      document.addEventListener('touchmove', onTouchMove);
-      document.addEventListener('touchend', onTouchEnd);
-    }
-  }
-
   bind() {
     this._taskList = this.element.querySelector('.taskList__list');
     this._taskList.addEventListener('keyup', this._onKeyUp);
-    this._taskList.addEventListener('mousedown', this._onMouseDown);
-    this._taskList.addEventListener('touchstart', this._onTouchStart);
+    this._taskList.addEventListener('mousedown', this._onPress);
+    this._taskList.addEventListener('touchstart', this._onPress);
   }
 
   unbind() {
     this._taskList.removeEventListener('keyup', this._onKeyUp);
-    this._taskList.removeEventListener('mousedown', this._onMouseDown);
-    this._taskList.removeEventListener('touchstart', this._onTouchStart);
+    this._taskList.removeEventListener('mousedown', this._onPress);
+    this._taskList.removeEventListener('touchstart', this._onPress);
   }
 
   onTaskClick() {}
